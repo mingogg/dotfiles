e754b4fb77b282c2530361a79f6e953df7409a01
mingo <mingogadeapy@gmail.com>
2025-12-14 13:04:29 -0300

    fix typo in plugin/theme.lua

--- config/nvim/lua/config/plugins/theme.lua
+++ config/nvim/lua/config/plugins/theme.lua
@@ -1,7 +1,7 @@
 local theme_path = vim.fn.expand("~/.config/theme/current/nvim/theme.lua")
 
 if vim.fn.filereadable(theme_path) == 1 then
-  return dotfile(theme_path)
+  return dofile(theme_path)
 end
 
 return {}
