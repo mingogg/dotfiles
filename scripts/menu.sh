102c0311a8082f4cc29e046a1ddfe5f8861c1864
mingo <mingogadeapy@gmail.com>
2025-12-15 18:02:41 -0300

    make scripts executable & add waybar menu

---
+++ scripts/menu.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+SCRIPTS_DIR="$HOME/dotfiles/scripts"
+
+options=$(cat <<EOF
+🎨  Style: Change Theme
+⏻  System: Logout
+🔁  System: Reboot
+⏹  System: Shutdown
+EOF
+)
+
+selected=$(echo "$options" | walker -d -p "Go...")
+
+[ -z "$selected" ] && exit 0
+
+case "$selected" in
+  "🎨  Style: Change Theme")
+    "$SCRIPTS_DIR/change_theme.sh"
+    ;;
+  "⏻  System: Logout")
+    hyprctl dispatch exit
+    ;;
+  "🔁  System: Reboot")
+    systemctl reboot
+    ;;
+  "⏹  System: Shutdown")
+    systemctl poweroff
+    ;;
+esac
