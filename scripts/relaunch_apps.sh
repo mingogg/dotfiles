719e2a0741ff35d411ac6ff3134e16ff1cb5361e
mingo <mingogadeapy@gmail.com>
2025-12-28 14:19:00 -0300

    update: ly colors for orange theme

--- scripts/relaunch_apps.sh
+++ scripts/relaunch_apps.sh
@@ -2,16 +2,22 @@
 
 CURRENT="$HOME/.config/theme/current"
 
+# Hyprland
 hyprctl reload
 
+# Bashrc / alacritty
 source ~/.bashrc
 
+# Wallpaper
 pkill swaybg
 sleep 0.1
 nohup swaybg -i "$CURRENT/wallpaper/wallpaper.jpg" -m fill >/dev/null 2>&1 &
 disown
 
+# Waybar
 pkill waybar
 sleep 0.1
 nohup waybar >/dev/null 2>&1 &
 disown
+
+# Ly login manager service will update the theme after rebooting the system
