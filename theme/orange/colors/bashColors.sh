96fd49f68f4e66b7eb5489fd0c2343ac55173800
mingo <mingogadeapy@gmail.com>
2025-12-28 10:35:29 -0300

    add: orange theme for all apps

--- theme/orange/colors/bashColors.sh
+++ theme/orange/colors/bashColors.sh
@@ -1,7 +1,9 @@
 BG="\e[48;2;30;30;30m"           # background
 FG="\e[38;2;138;138;141m"        # foreground
-PRIMARY="\e[38;2;23;147;209m"    # primary
-ACCENT="\e[38;2;102;217;255m"    # accent
 RESET="\e[0m"
-GIT_CLEAN="\e[38;2;102;217;255m" # accent for clean git
+
+PRIMARY="\e[38;2;230;126;34m"    # primary
+ACCENT="\e[38;2;245;176;65m"     # accent
+
+GIT_CLEAN="\e[38;2;245;176;65m"  # accent for clean git
 GIT_DIRTY="\e[38;2;217;102;102m" # soft for git changes
