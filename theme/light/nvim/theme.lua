b9f9c36f886efa9ce5000b8b1e626d1f2e035b39
mingo <mingogadeapy@gmail.com>
2025-12-28 15:52:56 -0300

    fix: nvim light theme

--- theme/light/nvim/theme.lua
+++ theme/light/nvim/theme.lua
@@ -1,14 +1,16 @@
-{
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.g.tokyonight_style = "night"
-    vim.g.tokyonight_colors = {
-      fg = "#e5e5e5",
-      fg_dark = "#bfbfbf",
-      border = "#ffffff",
-    }
-    vim.cmd.colorscheme("tokyonight")
-  end,
-}
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.tokyonight_style = "night"
+      vim.g.tokyonight_colors = {
+        fg = "#e5e5e5",
+        fg_dark = "#bfbfbf",
+        border = "#ffffff",
+      }
+      vim.cmd.colorscheme("tokyonight")
+    end,
+  },
+ }
