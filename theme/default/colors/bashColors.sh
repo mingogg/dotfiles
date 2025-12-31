046e5c94ac059a87fedf9fc6fe6555fe198632d0
mingo <mingogadeapy@gmail.com>
2025-12-31 20:52:36 -0300

    update: rm redundance in git clean & accent colors

--- theme/default/colors/bashColors.sh
+++ theme/default/colors/bashColors.sh
@@ -5,5 +5,4 @@ RESET="\e[0m"
 PRIMARY="\e[38;2;23;147;209m"    # primary
 ACCENT="\e[38;2;102;217;255m"    # accent
 
-GIT_CLEAN="\e[38;2;102;217;255m" # accent for clean git
 GIT_DIRTY="\e[38;2;217;102;102m" # soft for git changes
