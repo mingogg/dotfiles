d7f6f2f0ac221e6c705663cbf0c64e3391dc4111
mingo <mingogadeapy@gmail.com>
2026-01-03 18:50:23 -0300

    add: nautilus config script

---
+++ theme/light/nautilus/apply.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-Grey'
+gsettings set org.gnome.desktop.interface icon-theme 'Yaru-Grey'
---
+++ theme/orange/nautilus/apply.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-Orange'
+gsettings set org.gnome.desktop.interface icon-theme 'Yaru-Orange'
