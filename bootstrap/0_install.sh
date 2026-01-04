5d5af49601dedaf16881237a7542b1b27fe0ba2e
mingo <mingogadeapy@gmail.com>
2026-01-03 21:54:23 -0300

    fix: grub fallback

--- bootstrap/0_install.sh
+++ bootstrap/0_install.sh
@@ -13,32 +13,39 @@ echo "Installing base system (pacstrap)..."
 pacstrap /mnt base linux linux-firmware base-devel networkmanager git nano
 
 echo "Generating fstab..."
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt > /mnt/etc/fstab
 
 echo "Configuring base system..."
 arch-chroot /mnt /bin/bash <<EOF
 # Localization:
 ln -sf /usr/share/zoneinfo/America/Asuncion /etc/localtime
 hwclock --systohc
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+
+if ! grep -q "en_US.UTF-8 UTF-8" /etc/locale.gen; then
+  echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+fi
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-# Host name
+# Setting the host name
 echo "$HOSTNAME" > /etc/hostname
 
 # Installation of bootloader (UEFI)
 # Note: Asumes it is mounted the EFI partition in /mnt/boot
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+# First: normal entry
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
+# After: compatibility entry (removable) for rescue
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --removable
+
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Root password
+# Setting root password
 echo "Set the password for ROOT user for the new system:"
 passwd
 EOF
 
 echo "----------------------------------------------------"
 echo "LEVEL 0 PROCESS IS FINISHED."
-echo "Host set as: $HOSTNAME_VAR"
+echo "Host set as: $HOSTNAME"
 echo "You can now execute: umount -R /mnt && reboot"
 echo "----------------------------------------------------"
